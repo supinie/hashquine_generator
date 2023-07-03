f2ad8146d0f78fa42430cca0e90304e7520c802d
supinie <joshua@supinie.mozmail.com>
2023-07-03 14:35:09 +0100

    .

--- lib/read_data.go
+++ lib/read_data.go
@@ -1,31 +1,5 @@
 package lib
 
-// import (
-// 	"fmt"
-// 	"os"
-// )
-
-// func Read_gif(filename string) (map[string]([]byte), error) {
-//     empty := make(map[string]([]byte))
-//     file_data, err := os.ReadFile(filename)
-//     if err != nil {
-//         return empty, err
-//     }
-//     blocks := make(map[string]([]byte))
-
-//     blocks["header"], file_data  = file_data[:6], file_data[6:]
-//     blocks["lcd"], file_data = file_data[:7], file_data[7:]
-//     blocks["gct"], file_data = file_data[:16 * 3], file_data[(16 * 3):]
-//     blocks["img_descriptor"], file_data = file_data[:10], file_data[10:]
-//     blocks["img_data"], file_data = file_data[:1], file_data[1:]
-//     for i := 0; i < 50; i++ {
-//         new_block := make([]byte, 0)
-//         new_block, file_data = file_data[:1], file_data[1:]
-//         fmt.Printf("%x\n", new_block)
-//     }
-//     return blocks, err
-// }
-
 import (
 	"os"
     "fmt"
@@ -56,9 +30,9 @@ func Read_gif(filename string) (map[string][]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-    fmt.Printf("%x\n", lcd)
 	if !bytes.HasSuffix(lcd, []byte{0x80, 0x02, 0x00}) {
-		return nil, fmt.Errorf("Invalid logical screen descriptor")
+        fmt.Printf("%x\n", lcd)
+		return nil, fmt.Errorf("Invalid logical screen descriptor for %s", filename)
 	}
 	blocks["lcd"] = lcd
 
@@ -74,9 +48,9 @@ func Read_gif(filename string) (map[string][]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	if imgDescriptor[0] != 0x2c || imgDescriptor[9] != 0 {
-		return nil, fmt.Errorf("Invalid image descriptor")
-	}
+	// if imgDescriptor[0] != 0x2c || imgDescriptor[9] != 0 {
+	// 	return nil, fmt.Errorf("Invalid image descriptor")
+	// }
 	blocks["img_descriptor"] = imgDescriptor
 
 	imgData := make([]byte, 1)
@@ -90,7 +64,8 @@ func Read_gif(filename string) (map[string][]byte, error) {
 		if err != nil {
 			return nil, err
 		}
-		imgData = append(imgData, subBlockSize...)
+        fmt.Printf("%x\n", subBlockSize)
+		imgData = append(imgData, subBlockSize[0])
 		if subBlockSize[0] == 0 {
 			break
 		}
@@ -99,16 +74,18 @@ func Read_gif(filename string) (map[string][]byte, error) {
 		if err != nil {
 			return nil, err
 		}
+        fmt.Printf("%x\n", subBlockData)
 		imgData = append(imgData, subBlockData...)
 	}
 	blocks["img_data"] = imgData
 
-	trailer := make([]byte, 1)
+	trailer := make([]byte, 2)
 	_, err = file.Read(trailer)
 	if err != nil {
 		return nil, err
 	}
-	if trailer[0] != 0x3b {
+    fmt.Printf("%x\n", trailer)
+	if trailer[1] != 0x3b {
 		return nil, fmt.Errorf("Invalid GIF trailer")
 	}
 
