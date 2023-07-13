58d82e4e707fde5e96049704dead71f1485f01b2
supinie <joshua@supinie.mozmail.com>
2023-07-13 21:27:22 +0100

    readme

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
+# Go md5 Hashquine Generator
+
+This is a script to generate a gif that will display its own md5sum.
+
+It is a Go implementation of [Rogdham's python script](https://github.com/rogdham/gif-md5-hashquine). The script works by generating a collision for all possible characters at each possible position, for both visable and hidden. Once we have finished, we can then selectively choose to display characters based on the md5sum of the file, without changing it.
+
+Here is an example generation:
+
+![hashquine](./hashquine.gif)
+
+which can be seen to be displaying its own md5 hash:
+
+```
+┌[supinie@ubuntuVM] [/dev/pts/2] [main ⚡] 
+└[~/git/hashquine]> md5sum hashquine.gif 
+18f6d7bae1ca24b0ea3224560f046cd9  hashquine.gif
+```
+
 ## Prerequisites:
 
 You must have fastcoll, this can be installed by the following:
--- lib/generate.go
+++ lib/generate.go
@@ -105,6 +105,85 @@ func test_file_hashes(filename1, filename2 string) (bool, error) {
     return bytes.Equal(h1.Sum(nil), h2.Sum(nil)), err 
 }
 
+func add_comment() []byte {
+    comment := []byte("If youre reading this, please dont use md5\n")
+    comment_block := []byte{0x21, 0xfe}
+    comment_block = append(comment_block, byte(len(comment)))
+    comment_block = append(comment_block, comment...)
+    comment_block = append(comment_block, 0x00)
+    return comment_block
+}
+
+func add_image_descriptor(left, top int, hp *Hashquine_params) []byte {
+    left_bytes := make([]byte, 2)
+    top_bytes := make([]byte, 2)
+    width_bytes := make([]byte, 2)
+    height_bytes := make([]byte, 2)
+    binary.LittleEndian.PutUint16(left_bytes, uint16(left))
+    binary.LittleEndian.PutUint16(top_bytes, uint16(top))
+    binary.LittleEndian.PutUint16(width_bytes, uint16(hp.Char_width))
+    binary.LittleEndian.PutUint16(height_bytes, uint16(hp.Char_height))
+    img_descriptor := left_bytes
+    img_descriptor = append(img_descriptor, top_bytes...)
+    img_descriptor = append(img_descriptor, width_bytes...)
+    img_descriptor = append(img_descriptor, height_bytes...)
+    img_descriptor = append(img_descriptor, 0x00)
+    return img_descriptor
+}
+
+func calc_padding(coll_img, coll_nop []byte, current_coll_size int) (int, int) {
+    offset := COLLISION_LEN - COLLISION_DIFF - 1
+    coll_p_img := int(coll_img[COLLISION_DIFF]) - offset
+    coll_p_nop := int(coll_nop[COLLISION_DIFF]) - offset
+    pad_len := coll_p_nop - coll_p_img - current_coll_size - 4
+    if pad_len < 0 {
+        pad_len += 64
+    }
+    return pad_len, coll_p_img
+}
+
+func brute_force_mask(md5sum []byte, hp *Hashquine_params) []byte {
+    for garbage := 0; garbage < (1 << 32); garbage++ {
+        end := []byte{0x04, byte(garbage), 0x00, 0x3b}
+
+        new_md5 := md5.Sum(append(md5sum[:], end...))
+
+        match := true
+        new_md5_iterable := []rune(fmt.Sprintf("%x", new_md5))
+        for i, mask_char := range hp.Mask {
+            md5_char := new_md5_iterable[i]
+            if string(mask_char) != " " && string(mask_char) != string(md5_char) {
+                match = false
+                break
+            }
+        }
+
+        if match {
+            fmt.Println("Found bruteforce")
+            return end
+        }
+    }
+    return nil
+}
+
+func select_hash_chars(hp *Hashquine_params, generated_gif []byte, alternatives map[Alternative_Key]Alternative_Value) ([]byte, error) { 
+    hash_rune_slice := []rune(fmt.Sprintf("%x", md5.Sum(generated_gif)))
+    mask_rune_slice := []rune(hp.Mask)
+    for i, char := range hash_rune_slice {
+        if string(mask_rune_slice[i]) != " " {
+            continue
+        }
+        x := i % 8
+        y := i / 8
+        new_char, err := strconv.ParseInt(string(char), 16, 0)
+        if err != nil {
+            return nil, err
+        }
+        coll_alternative := alternatives[Alternative_Key{[2]int{x, y}, int(new_char)}]
+        generated_gif = append(generated_gif[:coll_alternative.Coll_pos], append(coll_alternative.Coll, generated_gif[coll_alternative.Coll_pos + len(coll_alternative.Coll):]...)...)
+    }
+    return generated_gif, nil
+}
 
 func Generate(hp *Hashquine_params) ([]byte, error) {
     graphic_control_extension := []byte{0x21, 0xf9, 0x04, 0x04, 0x02, 0x00, 0x00, 0x00}
@@ -114,11 +193,8 @@ func Generate(hp *Hashquine_params) ([]byte, error) {
     generated_gif := append(hp.Background_blocks["header"], hp.Background_blocks["lsd"]...)
     generated_gif = append(generated_gif, hp.Background_blocks["gct"]...)
     
-    comment := []byte("If youre reading this, please dont use md5\n")
-    generated_gif = append(generated_gif, []byte{0x21, 0xfe}...)
-    generated_gif = append(generated_gif, byte(len(comment)))
-    generated_gif = append(generated_gif, comment...)
-    generated_gif = append(generated_gif, 0x00)
+    comment_block := add_comment()
+    generated_gif = append(generated_gif, comment_block...)
 
     generated_gif = append(generated_gif, graphic_control_extension...)
     generated_gif = append(generated_gif, hp.Background_blocks["img_descriptor"]...)
@@ -145,19 +221,8 @@ func Generate(hp *Hashquine_params) ([]byte, error) {
             for char := 0; char < 16; char++ {
                 char_img := append(graphic_control_extension, []byte{0x2c}...)
 
-                left_bytes := make([]byte, 2)
-                top_bytes := make([]byte, 2)
-                width_bytes := make([]byte, 2)
-                height_bytes := make([]byte, 2)
-                binary.LittleEndian.PutUint16(left_bytes, uint16(left))
-                binary.LittleEndian.PutUint16(top_bytes, uint16(top))
-                binary.LittleEndian.PutUint16(width_bytes, uint16(hp.Char_width))
-                binary.LittleEndian.PutUint16(height_bytes, uint16(hp.Char_height))
-                char_img = append(char_img, left_bytes...)
-                char_img = append(char_img, top_bytes...)
-                char_img = append(char_img, width_bytes...)
-                char_img = append(char_img, height_bytes...)
-                char_img = append(char_img, 0x00)
+                img_descriptor := add_image_descriptor(left, top, hp)
+                char_img = append(char_img, img_descriptor...)
                 
                 char_img = append(char_img, hp.Chars_img_data[char]...)
 
@@ -175,13 +240,7 @@ func Generate(hp *Hashquine_params) ([]byte, error) {
                     if err != nil {
                         return nil, err
                     }
-                    offset := COLLISION_LEN - COLLISION_DIFF - 1
-                    coll_p_img = int(coll_img[COLLISION_DIFF]) - offset
-                    coll_p_nop := int(coll_nop[COLLISION_DIFF]) - offset
-                    pad_len = coll_p_nop - coll_p_img - len(char_img) - 4
-                    if pad_len < 0 {
-                        pad_len += 64
-                    }
+                    pad_len, coll_p_img = calc_padding(coll_img, coll_nop, len(char_img))
                     if coll_p_img >= 0 && pad_len >= 0 {
                         break
                     }
@@ -203,49 +262,19 @@ func Generate(hp *Hashquine_params) ([]byte, error) {
     }
     current_md5 := md5.Sum(generated_gif)
 
-    fmt.Println("Brute forcing...")
-    for garbage := 0; garbage < (1 << 32); garbage++ {
-        end := []byte{0x04, byte(garbage), 0x00, 0x3b}
-
-        new_md5 := md5.Sum(append(current_md5[:], end...))
-
-        match := true
-        new_md5_iterable := []rune(fmt.Sprintf("%x", new_md5))
-        for i, mask_char := range hp.Mask {
-            md5_char := new_md5_iterable[i]
-            if string(mask_char) != " " && string(mask_char) != string(md5_char) {
-                match = false
-                break
-            }
-        }
-
-        if match {
-            fmt.Println("Found bruteforce")
-            generated_gif = append(generated_gif, end...)
-            break
-        }
-    }
-    if len(generated_gif) == 0 { 
+    fmt.Println("\nBrute forcing...")
+    end := brute_force_mask(current_md5[:], hp)
+    if end == nil { 
         err := errors.New("Did not find GIF matching md5 mask")
         return nil, err
     }
+    generated_gif = append(generated_gif, end...)
+
 
     fmt.Printf("Target hash: %x", md5.Sum(generated_gif))
-    hash_rune_slice := []rune(fmt.Sprintf("%x", md5.Sum(generated_gif)))
-    mask_rune_slice := []rune(hp.Mask)
-    for i, char := range hash_rune_slice {
-        if string(mask_rune_slice[i]) != " " {
-            continue
-        }
-        x := i % 8
-        y := i / 8
-        new_char, err := strconv.ParseInt(string(char), 16, 0)
-        if err != nil {
-            return nil, err
-        }
-        coll_alternative := alternatives[Alternative_Key{[2]int{x, y}, int(new_char)}]
-        fmt.Printf("Substituting char %v in position (%v, %v)\n", new_char, x, y)
-        generated_gif = append(generated_gif[:coll_alternative.Coll_pos], append(coll_alternative.Coll, generated_gif[coll_alternative.Coll_pos + len(coll_alternative.Coll):]...)...)
+    generated_gif, err = select_hash_chars(hp, generated_gif, alternatives)
+    if err != nil {
+        return nil, err
     }
     return generated_gif, err
 }
